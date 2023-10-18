bcd7534707f12be313d1be2a319cf35ffeb2c4f6
VadimDariy <Metallika_1979@mail.ru>
2023-10-18 18:59:48 +0300

    Решение задачи 1.1.5 по Hibernate.

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,18 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private Session session;
+    private Transaction transaction;
+
     public UserDaoHibernateImpl() {
 
     }
@@ -12,31 +20,90 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.createSQLQuery("CREATE TABLE IF NOT EXISTS User (" +
+                    "id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT," +
+                    "name VARCHAR (50) NOT NULL," +
+                    "lastName VARCHAR (50) NOT NULL," +
+                    "age TINYINT NOT NULL)").executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            transaction.rollback();
+        }
     }
 
     @Override
     public void dropUsersTable() {
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.createSQLQuery("DROP TABLE IF EXISTS User")
+                    .executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            transaction.rollback();
+        }
 
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
 
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        User user = new User(name, lastName, age);
+        try {
+            session.save(user);
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            transaction.rollback();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.delete(session.get(User.class, id));
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            transaction.rollback();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        Session session = Util.getSessionFactory().openSession();
+        List list = new ArrayList<>();
+        Transaction transaction = session.beginTransaction();
+        try {
+            list = session.createQuery("FROM User ").list();
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            transaction.rollback();
+        }
+        return list;
     }
 
     @Override
     public void cleanUsersTable() {
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.createSQLQuery("TRUNCATE TABLE User").executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            transaction.rollback();
+        }
 
     }
 }
--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,13 +1,14 @@
 package jm.task.core.jdbc.model;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 @Table
-public class User {
+public class User implements Serializable {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
